a267f5d84c7878cce2fec75d771661470fa7a389
kayesmahmud000 <mdkayesmahmud3@gmail.com>
2025-02-19 01:52:45 +0600

    added the update user data section

--- src/pages/Dashboard/Common/UpdateProfile.jsx
+++ src/pages/Dashboard/Common/UpdateProfile.jsx
@@ -4,14 +4,14 @@ const UpdateProfile = () => {
     return (
         <div>
         <div className='lg:min-h-[730px]'>
-         <div className='max-w-3xl  mx-auto my-10 p-5 py-10 flex flex-col rounded-lg justify-center items-center gap-6 border '>
+         <div className='max-w-3xl  mx-auto my-10 p-5 py-10 flex flex-col rounded-lg justify-center items-center gap-6 bg-[#1e172b]  '>
              <h1 className='text-3xl font-bold text-center'> Update Your Information </h1>
-             <div className="card bg-base-100 w-full max-w-xl shrink-0 ">
+             <div className="card w-full max-w-xl shrink-0 ">
 
              <form   className="card-body">
                  <div className="form-control">
                      <label className="label">
-                         <span className="label-text text-lg">Name</span>
+                         <span className="label-text text-white text-lg">Name</span>
                      </label>
                      <input type="text"
                       placeholder="name"
@@ -20,7 +20,7 @@ const UpdateProfile = () => {
                  </div>
                  <div className="form-control">
                      <label className="label">
-                         <span className="label-text text-lg">Photo URL</span>
+                         <span className="label-text text-white text-lg">Photo URL</span>
                      </label>
                      <input type="text" 
                      name="photo"
@@ -28,7 +28,12 @@ const UpdateProfile = () => {
                      className="input input-bordered" required />
                  </div>
                  <div className="form-control mt-6">
-                 <button className='btn bg-[#A020F0] px-4 text-white rounded-full'> Update Information</button>
+                 <button
+                    className="mt-6 bg-purple-600 hover:bg-yellow-400 hover:text-black text-white font-semibold py-2 px-6 rounded-full transition duration-300"
+                >
+                   Update Information
+                </button>
+                
                  </div>
              </form>
            
