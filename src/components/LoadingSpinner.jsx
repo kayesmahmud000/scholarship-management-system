cbd9dc3d8e9788522ae8b0713c69da7540607e1c
kayesmahmud000 <mdkayesmahmud3@gmail.com>
2025-09-13 14:26:46 +0600

    fixt the build pblm

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,21 +1,22 @@
-import { DotLoader } from "react-spinners";
+import DotLoader from "react-spinners/DotLoader";
 import PageHelmet from "./PageHelmet";
 
-
 const LoadingSpinner = () => {
-    return (
-        <div
-        className={` min-h-screen items-center justify-center}
+  return (
+    <div
+      className="
+        min-h-screen 
         flex 
         flex-col 
         justify-center 
-        items-center `}
-      >
-        <PageHelmet title={'Loading..'}/>
-        <DotLoader size={80} color='purple' />
-        {/* <ScaleLoader size={100} color='lime' /> */}
-      </div>
-    );
+        items-center
+      "
+    >
+      <PageHelmet title={'Loading..'} />
+      <DotLoader size={80} color="purple" />
+      {/* <ScaleLoader size={100} color="lime" /> */}
+    </div>
+  );
 };
 
 export default LoadingSpinner;
